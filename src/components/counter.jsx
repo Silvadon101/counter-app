aba795b20e12c8622bba4c25a100f7dc6c6cbeff
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2022-02-03 01:44:19 -0500

    Setting Attributes
    Setting dynamic attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    //?  This includes data for your components
+    //? e.g. images, files, etc
+
+    count: 10,
+    imageUrl: 'https://picsum.photos/200'
+
   };
 
   render() {
     return (
       <React.Fragment>
+          <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
@@ -16,7 +22,7 @@ class Counter extends Component {
 
   formatCount(){
       const {count} = this.state;
-      return count === 0 ? 'Zero' : count;
+      return count === 0 ? <b>Zero</b> : <b>{count}</b>;
   }
 }
 
