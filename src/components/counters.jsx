4cc3464f01667d7a6d1ce0316851568c40996bba
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2022-02-18 00:02:37 -0500

    Different Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -8,24 +8,47 @@ class Counters extends Component {
     // -----------------------------------------------------
 
     counters: [
-      { id: 1, value: 5 },
-      { id: 2, value: 0 },
-      { id: 3, value: 1 },
-      { id: 4, value: 0 },
+      { id: 1, value: 5, desc: "cool" },
+      { id: 2, value: 0, desc: "cool" },
+      { id: 3, value: 1, desc: "cool" },
+      { id: 4, value: 0, desc: "cool" },
     ],
   };
 
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(c => c.id !== counterId)
-    this.setState({ counters: counters }) 
-    //* Or
-    //? this.setState({ counters }) 
+  handleReset = () => {
+    /**-----------------------------------------
+     * @param c
+     * the 'c' here represents 'counters'property.
+     * It is being mapped using the 'map()'where
+     * all its array keys are represented by the 'c'
+     * in this case.
+     * -----------------------------------------
+     */
 
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+    //* Or
+    //? this.setState({ counters })
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
